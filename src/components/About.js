7563fc554749de2d49e943f12d429790b6e1eff1
FauFau <faufau241@gmail.com>
2022-05-14 15:32:58 +0700

    update about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './LandingPageCompt/Body/Body.css'
+import {Container, Row, Col, Carousel} from 'react-bootstrap';
+import Logo from '../assets/images/logo2.png';
+import { Footer } from './LandingPageCompt/Footer/Footer'
+import AboutCard from './AboutCard';
 
 function About() {
   return (
     <div>
-        <h1>About</h1>
+      <div className='container'>
+      <h1>Our Team</h1>
+      <AboutCard/>
+      </div>
+        <div className="aboutus">
+            <h2>About us</h2>
+                <Row>
+                    <Col>
+                        <p>Vitour merupakan website pariwisata berbasis virtual yang memudahkan orang untuk berpergian mengunjungi tempat wisata tanpa harus mendatanginya serta didukung informasi tentang kebudayaan di tiap daerah di Indonesia</p>
+                        <p>VITOUR akan menjadi solusi alternatif bagi anda yang ingin melakukan rekreasi tanpa khawatir akan keterbatasan waktu, biaya dan tempat. 
+                           Dalam jangka panjang VITOUR juga dapat meningkatkan pengetahuan masyarakat mengenai pariwisata lokal.</p>
+                    </Col>
+                    <Col>
+                        <div className="img-wrap">
+                            <img src={Logo} alt="" />
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        <Footer />
     </div>
   )
 }
