5cc7bb8462d537efe9506d5f60a38dc845e9590f
FauFau <faufau241@gmail.com>
2022-05-31 09:51:57 +0700

    update lorem ipsmu

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -10,47 +10,45 @@ const AboutCard = ()=>{
         <>
         <div class="card-group">
         <div class="card">
-        <img src={FotoKita1} class="mx-auto card-img-fluid" alt="..." width={300} height={300}/>
+        <img src={FotoKita1} class="mx-auto card-img-fluid" alt="..." width={250} height={300}/>
           <div class="card-body">
             <h5 class="card-title">Farhan Gunadi</h5>
-            <p class="card-text">Developer</p>
+            <p class="card-text">Front End</p>
           </div>
         </div>
         <div class="card">
-        <img src={FotoKita2} class="mx-auto card-img-fluid" alt="..." width={300} height={300}/>
+        <img src={FotoKita2} class="mx-auto card-img-fluid" alt="..." width={250} height={300}/>
           <div class="card-body">
             <h5 class="card-title">Raihan Fatoni</h5>
-            <p class="card-text">Developer</p>
+            <p class="card-text">Back End</p>
           </div>
         </div>
         <div class="card">
-        <img src={FotoKita3} class="mx-auto card-img-fluid" alt="..." width={300} height={300}/>
+        <img src={FotoKita3} class="mx-auto card-img-fluid" alt="..." width={250} height={300}/>
           <div class="card-body">
             <h5 class="card-title">Aidil Fitra</h5>
-            <p class="card-text">Developer</p>
+            <p class="card-text">Back End</p>
           </div>
         </div>
       </div>
       
 
-
-
       <div class="row row-cols-1 row-cols-md-2 g-2">
   <div class="col">
     <div class="card">
-    <img src={FotoKita4} class=" mx-auto card-img-fluid" alt="..." width={300} height={300} />
+    <img src={FotoKita4} class=" mx-auto card-img-fluid" alt="..." width={250} height={300} />
       <div class="card-body">
         <h5 class="card-title">Andre Luckyta Firdaus</h5>
-        <p class="card-text">Developer</p>
+        <p class="card-text">Project Manager</p>
       </div>
     </div>
   </div>
   <div class="col">
     <div class="card">
-    <img src={FotoKita5} class=" mx-auto card-img-fluid" alt="..." width={300} height={300} />
+    <img src={FotoKita5} class=" mx-auto card-img-fluid" alt="..." width={250} height={300} />
       <div class="card-body">
         <h5 class="card-title">Muhammad Alwan Fauzi</h5>
-        <p class="card-text">Developer</p>
+        <p class="card-text">UI/UX Designer</p>
       </div>
     </div>
   </div>
