0f76dd1ea6e831654db1db850e11d7e43b1e0458
aidilfitra08 <aidil.gitu1@gmail.com>
2022-11-12 15:53:50 +0700

    Logout component

---
+++ src/components/LoginRegister/Logout.js
@@ -0,0 +1,26 @@
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import "./../Navbar.css";
+
+function Logout () {
+    
+    let navigate = useNavigate();
+
+    
+
+    const logout = () => {
+        // localStorage.removeItem('token-info');
+        sessionStorage.clear();
+        // setIsLoggedin(false);
+        navigate('/');
+    };
+
+    return (
+        <Button onClickCapture={logout} className="logout">
+            Logout
+        </Button>
+    )
+    // window.location.reload(true);
+}
+
+export default Logout
