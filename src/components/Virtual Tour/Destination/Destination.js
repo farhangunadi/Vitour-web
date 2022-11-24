e48a52972dd8016b620b3db1c9053839a2b6cef5
farhangunadi <farhan19004@mail.unpad.ac.id>
2022-11-24 20:56:05 +0700

    Fix some component

--- src/components/Virtual Tour/Destination/Destination.js
+++ src/components/Virtual Tour/Destination/Destination.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 //style
 import "./Destination.css";
@@ -16,6 +16,7 @@ function Destination() {
 
   const [destination, setDestination] = useState([]);
   const [video, setVideo] = useState([]);
+  const [image, setImage] = useState([]);
   const [handle, setHandle] = useState(false);
   const [photos, setPhotos] = useState([]);
 
@@ -24,53 +25,55 @@ function Destination() {
       axios
         .get(`https://vitour-backend.herokuapp.com/api/city/destinations/${id}`)
         .then((res) => {
-          console.log("result :", res.data.data[0].videovrs.length);
-          setDestination(res.data.data[0]);
-          if(res.data.data[0].videovrs.length != 0) {
-            setVideo(res.data.data[0].videovrs[0].link_video);
+          console.log("result :", res.data.data);
+          console.log("result :", res.data.data.videovrs.length);
+          setDestination(res.data.data);
+          if (res.data.data.videovrs.length != 0) {
+            setVideo(res.data.data.videovrs[0].link_video);
             setHandle(true);
           } else {
             setVideo("");
             setHandle(false);
           }
           // setVideo(res.data.data[0].videovrs[0].link_video);
-          setPhotos(res.data.data[0].images)
-          
-          console.log(res.data.data[0].images)
+          setPhotos(res.data.data.images);
+
+          console.log(res.data.data.images);
         });
     };
 
     fetchDestination();
   }, [id]);
-
   return (
     <>
       <section className="header_destination">
         <div className="img_wrap">
-          <img
-            src="https://ngonoo.com/engine/wp-content/uploads/2014/12/Indahnya-Alam-Wallpaper.jpg"
-            alt=""
-            className="header_background"
-          />
+          {photos.slice(1, 2).map((image) => {
+            return (
+              <img
+                src={image.images_link}
+                alt=""
+                className="header_background"
+              />
+            );
+          })}
         </div>
         <div className="header_content_destination">
           <h1 className="title_destination">{destination.nama_destinasi}</h1>
           <p className="desc_destination">{destination.deskripsi_destinasi}</p>
         </div>
       </section>
-      
       <section className="top_destinasi">
-       <div className="rekomendasi_destinasi">
+        <div className="rekomendasi_destinasi">
           <h1 className="title_destination">Rekomendasi Tempat</h1>
           <p className="desc_destination">{destination.deskripsi_destinasi}</p>
-       </div>
+        </div>
       </section>
-
       <section className="wrap_video_destinasi">
         <iframe
           className="video_destinasi"
-          width="100%"
-          height="700"
+          width="946.67"
+          height="420"
           src={handle ? video : "https://www.youtube.com/embed/aKtb7Y3qOck"}
           title="YouTube video player"
           frameBorder="0"
@@ -81,15 +84,15 @@ function Destination() {
       <section className="gallery_destinasi">
         <h1 className="title_gallery">Destination Photos</h1>
         <div className="grid_gallery">
-        {photos.map((photo) => {
-          return (
-            <img
-              src= {photo.images_link}
-              alt="photo destinasi"
-              className="img_destination"
-            />
-          )
-        })}
+          {photos.map((photo) => {
+            return (
+              <img
+                src={photo.images_link}
+                alt="photo destinasi"
+                className="img_destination"
+              />
+            );
+          })}
 
           {/* <img
             src="https://jurnalsecurity.com/go/wp-content/uploads/2018/11/raja-ampat.jpg"
@@ -98,14 +101,19 @@ function Destination() {
           /> */}
         </div>
       </section>
-
       <section className="feedback">
-        <h1 className='title_feedback'>Customer Feedback</h1>
-            <Row xs="auto">
-              <Col><div className="divider-1"></div></Col>
-              <Col><div className="divider-2"></div></Col>
-              <Col><div className="divider-2"></div></Col>
-            </Row>
+        <h1 className="title_feedback">Customer Feedback</h1>
+        <Row xs="auto">
+          <Col>
+            <div className="divider-1"></div>
+          </Col>
+          <Col>
+            <div className="divider-2"></div>
+          </Col>
+          <Col>
+            <div className="divider-2"></div>
+          </Col>
+        </Row>
         <Container className="card-feedback">
           <Row xs="auto">
             <Col>
@@ -115,7 +123,10 @@ function Destination() {
                   alt=""
                   className="img-profile"
                 />
-                <p className="desc_feedback">Tempatnya sangat bagus dan murah. Sangat di rekomendasikan untuk keluarga</p>
+                <p className="desc_feedback">
+                  Tempatnya sangat bagus dan murah. Sangat di rekomendasikan
+                  untuk keluarga
+                </p>
                 <p className="profile_feedback">Ghaffa Gapuy</p>
                 <p className="profile_feedback">Manager</p>
               </div>
@@ -127,7 +138,10 @@ function Destination() {
                   alt=""
                   className="img-profile"
                 />
-                <p className="desc_feedback">Patut dikunjungi karena banyak menjual makanan enak dan spot foto</p>
+                <p className="desc_feedback">
+                  Patut dikunjungi karena banyak menjual makanan enak dan spot
+                  foto
+                </p>
                 <p className="profile_feedback">Dika Dikoy</p>
                 <p className="profile_feedback">Pejabat</p>
               </div>
